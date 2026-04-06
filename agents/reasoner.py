45d7e0aed9dfc48d75132bb64fde4eb6e9306804
FlyingMatrix <jxzhang.acm@gmail.com>
2026-04-06 17:03:56 +0200

    latest update

--- agents/reasoner.py
+++ agents/reasoner.py
@@ -7,8 +7,16 @@ class Reasoner:
         self.llm = OllamaLLM(model="llama3")
 
     def build_prompt(self, query: str, contexts):
+        """
+            the contexts is the response from the retriever agent -> a list of NodeWithScore objects
+
+            NodeWithScore(
+                node=<Node>,
+                score=<float>
+            )
+        """
         context_text = "\n\n".join(
-            [f"[{i}] {node.text}" for i, node in enumerate(contexts)]
+            [f"[{i}] {nws.node.get_content()}" for i, nws in enumerate(contexts)]
         )
 
         prompt = f"""
@@ -26,9 +34,10 @@ class Reasoner:
         return prompt
 
     def run(self, query: str):
+        # get the top_k most similar nodes wrapped with scores
         contexts = self.retriever.retrieve(query)
-
+        # use query and contexts to create prompt
         prompt = self.build_prompt(query, contexts)
-
+        # pass the prompt to llm to generate results
         return self.llm.stream_generate(prompt)
     
